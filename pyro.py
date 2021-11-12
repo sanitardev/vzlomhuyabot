8178aed44bcec4f42720fb8bafd144d651e56ba3
sanitardev <94179900+sanitardev@users.noreply.github.com>
2021-11-12 19:50:11 +0600

    Add files via upload

---
+++ pyro.py
@@ -0,0 +1,26 @@
+from pyrogram import Client, filters
+import time as t
+
+api_id = 17339211
+api_hash = "dac862dae6529516db1ca39d997470db"
+app = Client("my_account", api_id, api_hash)
+
+
+@app.on_message(filters.command(["vzlom"], prefixes="/"))
+def vzlom(client, message):
+		app.send_message(message.chat.id, 'Взлом хуя...🌀', reply_to_message_id=message.message_id)
+		t.sleep(1)
+		try:
+			app.edit_message_text(message.chat.id, message.message_id+1, f'**Хуй [{message.reply_to_message.from_user.first_name}](tg://user?id={message.reply_to_message.from_user.id}) успешно взломана!**')
+		except BaseException:
+			app.edit_message_text(message.chat.id, message.message_id+2, f'**Хуй [{message.reply_to_message.from_user.first_name}](tg://user?id={message.reply_to_message.from_user.id}) успешно взломана!**')
+
+@app.on_message(filters.command(["del"], prefixes="/"))
+def delcom(client, message):
+	try:
+		app.kick_chat_member(message.chat.id, message.reply_to_message.from_user.id)
+		app.send_message(message.chat.id, "Вон отсюда! Пользователь забанен.")	
+	except BaseException:
+		app.send_message(message.chat.id, "Ошибка")
+
+app.run()
